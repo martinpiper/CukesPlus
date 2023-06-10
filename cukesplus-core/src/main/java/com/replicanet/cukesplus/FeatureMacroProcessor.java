c7613d1c5ad32fbc9010dc10c767684b1fee1ca4
Martin Piper <martin.piper@gmail.com>
2023-06-10 19:03:38 +0800

    Syntax report missing "Given" for test1.macro: * this is a step definition with a simple parameter for * in * middle

--- cukesplus-core/src/main/java/com/replicanet/cukesplus/FeatureMacroProcessor.java
+++ cukesplus-core/src/main/java/com/replicanet/cukesplus/FeatureMacroProcessor.java
@@ -270,6 +270,7 @@ public class FeatureMacroProcessor
 
 				// Now we have a trimmed string in like the standard java step definition...
 				// @When("^I process the feature file \"([^\"]*)\" and the macro file \"([^\"]*)\"$")
+				macro.keyword = trimmed.split("[@(]")[1];
 				trimmed = trimRegex(trimmed);
 
 				macro.regexDefinition = trimmed;
